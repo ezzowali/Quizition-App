353b30d39ce5f7daa71052f069522fff0e562903
ezzowali <zwali.eng@gmail.com>
2021-10-01 20:13:59 +0300

    Quizition app Codes

---
+++ app/src/main/java/com/example/quizitionapp/show_subjectAdmin.java
@@ -0,0 +1,279 @@
+package com.example.quizitionapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.quizitionapp.R ;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class show_subjectAdmin extends AppCompatActivity {
+    private static final String apiurl = "http://192.168.1.2/Server/Subject.php";
+    ListView lv;
+
+    Button b_subj,b_add;
+
+
+    private static String subject[];
+    private static String timer[];
+    private static String first_date[];
+    private static String second_date[];
+    private static String first_time[];
+    private static String second_time[];
+
+
+    public static String gg;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_subject_admin);
+
+        lv = (ListView) findViewById( R.id.lv);
+
+        b_add=findViewById(R.id.b_add);
+
+        fetch_data_into_array(lv);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = lv.getItemAtPosition(position).toString();
+
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+
+        switch (item.getItemId()) {
+            case R.id.add:
+                startActivity(new Intent(show_subjectAdmin.this, add_subjectAdmin.class));
+                return true;
+            case R.id.back:
+                startActivity(new Intent(show_subjectAdmin.this, MainActivity.class));
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+
+
+
+
+        }
+    }
+
+    public void fetch_data_into_array(View view) {
+
+        class dbManager extends AsyncTask<String, Void, String> {
+            protected void onPostExecute(String data) {
+                try {
+                    JSONArray ja = new JSONArray(data);
+                    JSONObject jo = null;
+
+                    subject = new String[ja.length()];
+                    timer =new String[ja.length()];
+
+                    first_date = new String[ja.length()];
+                    second_date =new String[ja.length()];
+                    first_time = new String[ja.length()];
+                    second_time =new String[ja.length()];
+
+
+
+
+                    for (int i = 0; i < ja.length(); i++) {
+                        jo = ja.getJSONObject(i);
+                        subject[i] = jo.getString("Subject");
+                        timer[i] = jo.getString("timer");
+                        first_date[i]=jo.getString("first_date");
+                        second_date[i]=jo.getString("second_date");
+                        first_time[i]=jo.getString("first_time");
+                        second_time[i]=jo.getString("second_time");
+
+
+
+
+
+                    }
+
+                    myadapter adptr = new myadapter(getApplicationContext(), subject,timer,first_date,second_date,first_time,second_time);
+                    lv.setAdapter(adptr);
+
+                } catch (Exception ex) {
+                    Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuffer data = new StringBuffer();
+                    String line;
+
+                    while ((line = br.readLine()) != null) {
+                        data.append(line + "\n");
+                    }
+                    br.close();
+
+                    return data.toString();
+
+                } catch (Exception ex) {
+                    return ex.getMessage();
+                }
+
+            }
+
+        }
+        dbManager obj = new dbManager();
+        obj.execute(apiurl);
+
+    }
+
+    class myadapter extends ArrayAdapter<String> {
+        Context context;
+        String subject[];
+        String timer[];
+        String first_date[];
+        String second_date[];
+        String first_time[];
+        String second_time[];
+
+
+
+        myadapter(Context c, String subject[],
+                  String timer[],
+                  String first_date[],
+                String second_date[],
+                String first_time[],
+                String second_time[]) {
+            super(c, R.layout.subject_list, R.id.tv1, subject);
+            context = c;
+            this.subject = subject;
+            this.timer=timer;
+
+
+            this.first_date = first_date;
+            this.second_date=second_date;
+            this.first_time=first_time;
+            this.second_time=second_time;
+
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View subject_list = inflater.inflate(R.layout.subject_list, parent, false);
+
+
+            TextView tv1 = subject_list.findViewById(R.id.tv1);
+            TextView tv2 = subject_list.findViewById(R.id.tv2);
+            Button b_add=subject_list.findViewById(R.id.b_add);
+
+
+            TextView f_date = subject_list.findViewById(R.id.f_date);
+            TextView s_date = subject_list.findViewById(R.id.s_date);
+
+            TextView f_time = subject_list.findViewById(R.id.f_time);
+            TextView s_time = subject_list.findViewById(R.id.s_time);
+
+
+
+
+
+
+
+            tv1.setText(subject[position]);
+            tv2.setText(timer[position]);
+
+
+            f_date.setText(first_date[position]);
+            s_date.setText(second_date[position]);
+
+            f_time.setText(first_time[position]);
+            s_time.setText(second_time[position]);
+
+            b_add.setOnClickListener(new View.OnClickListener() {
+
+
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(show_subjectAdmin.this, write_Qus.class));
+
+                    Log.i("vcvcv", subject[position]);
+
+                    gg=subject[position];
+
+                }
+
+
+            });
+
+
+
+
+
+
+
+
+
+
+            return subject_list;
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/quizitionapp/write_Qus.java
@@ -0,0 +1,183 @@
+
+package com.example.quizitionapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizitionapp.R ;
+
+public class write_Qus extends AppCompatActivity {
+
+
+
+    Button b1, b2;
+    EditText ed1, ed2, ed3, ed4, ed5, ed;
+
+    String Subject = show_subjectAdmin.gg;
+
+
+    @SuppressLint("SimpleDateFormat")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_write__qus );
+
+
+
+        ed5 = findViewById( R.id.ed5 );
+        ed = findViewById( R.id.ed );
+        ed1 = findViewById( R.id.ed1 );
+        ed2 = findViewById( R.id.ed2 );
+        ed3 = findViewById( R.id.ed3 );
+        ed4 = findViewById( R.id.ed4 );
+        b1 = findViewById( R.id.b1 );
+        b2 = findViewById( R.id.button );
+
+        b2.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(write_Qus.this, show_subjectAdmin.class));
+                finish();
+            }
+        } );
+        b1.setOnClickListener( new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                final String Question, option1, option2, option3, option4, right_answer;
+
+
+                Question = String.valueOf( ed1.getText() );
+                option1 = String.valueOf( ed2.getText() );
+                option2 = String.valueOf( ed3.getText() );
+                option3 = String.valueOf( ed5.getText() );
+                option4 = String.valueOf( ed.getText() );
+                right_answer = String.valueOf( ed4.getText() );
+
+
+                if ((!Question.equals( "" )) && (!option1.equals( "" )) && (!option2.equals( "" )) &&
+                        (!right_answer.equals( "" )) && (!option3.equals( "" )) && (!option4.equals( "" ))) {
+                    Handler handler = new Handler();
+                    handler.post( new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[7];
+                            field[0] = "Question";
+                            field[1] = "option1";
+                            field[2] = "option2";
+                            field[3] = "option3";
+                            field[4] = "option4";
+                            field[5] = "right_answer";
+                            field[6] = "Subject";
+                            field[7]="image";
+
+//                            Log.i("bit", getStringImage(bitmap));
+
+
+
+
+
+
+
+                            //Creating array for data
+
+
+                            String[] data = new String[7];
+                            data[0] = Question;
+                            data[1] = option1;
+                            data[2] = option2;
+                            data[3] = option3;
+                            data[4] = option4;
+                            data[5] = right_answer;
+                            data[6] = Subject;
+
+
+
+                            PutData putData = new PutData( "http://192.168.1.2/Server/Qus_sub.php", "POST", field, data );
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+
+
+                                    char str = result.charAt( 7 );
+                                    Log.i( "PutData", String.valueOf( str ) );
+
+
+                                    if ("n".equalsIgnoreCase( String.valueOf( str ) )) {
+
+                                        Toast.makeText( getApplicationContext(), result, Toast.LENGTH_SHORT ).show();
+
+                                        startActivity( new Intent( write_Qus.this, write_Qus.class ) );
+                                        finish();
+                                    } else {
+
+                                        Toast.makeText( (getApplicationContext()), result, Toast.LENGTH_SHORT ).show();
+                                    }
+
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    } );
+
+
+                } else {
+                    Toast.makeText( getApplicationContext(), "error", Toast.LENGTH_SHORT ).show();
+                }
+
+
+            }
+
+
+        } );
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate( R.menu.back_write_qus, menu );
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+
+        switch (item.getItemId()) {
+
+            case R.id.back:
+                startActivity( new Intent( write_Qus.this, show_subjectAdmin.class ) );
+
+                return true;
+            default:
+                return super.onOptionsItemSelected( item );
+
+
+        }
+    }
+
+
+}
